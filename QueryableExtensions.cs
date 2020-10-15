dbf8f2586d1f2878e0eec400f962381be3de5e41
Valerii <valerii.savchenko@icloud.com>
2020-10-15 13:24:24 +0300

    Initial commit; Added IQueryable extensions

---
+++ QueryableExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace CabaVS.Shared.Extensions
+{
+    public static class QueryableExtensions
+    {
+        public static IQueryable<T> ApplyPredicate<T>(this IQueryable<T> expression, Expression<Func<T, bool>> predicate, bool isOptional = true)
+        {
+            if (expression == null) throw new ArgumentNullException(nameof(expression));
+            if (!isOptional && predicate == null) throw new ArgumentNullException(nameof(predicate));
+
+            var result = expression;
+
+            if (predicate != null)
+            {
+                result = result.Where(predicate);
+            }
+
+            return result;
+        }
+
+        public static IQueryable<T> SkipAndTake<T>(this IQueryable<T> expression, int skip, int take)
+        {
+            if (expression == null) throw new ArgumentNullException(nameof(expression));
+
+            var result = expression;
+
+            if (skip > 0) result = result.Skip(skip);
+            if (take > 0) result = result.Take(take);
+
+            return result;
+        }
+    }
+}
